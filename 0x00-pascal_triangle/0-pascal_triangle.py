cb7c33166556f40e484f5bb6a72e9aba85ee94a7
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-11-28 17:21:52 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-    """
+"""
 Contains the Pascal triangle implementation
 along with helper functions
 From the binomial expansion of (a + b)**n, to get the coefficients of terms we use nCr:
-These coefficients correspond to the numbers in the Pascal Triangle at row n. 
+These coefficients correspond to the numbers in the Pascal Triangle at row n.
+"""
+
+
+def pascal_triangle(n):
+    """Creates the pascal triangle with n rows
     """
+    pascal_list = []
+    for i in range(n):
+        pascal_list.append((list((nCr(i, j) for j in range(0, i + 1)))))
+    return pascal_list
+
 
 def factorial(n):
     """
@@ -13,24 +23,18 @@ def factorial(n):
     """
     result = 1
     if n == 0 and n == 1:
-	    return 1
+        return 1
     if n < 0:
-	    raise ValueError("No factorial of a negative number")
+        raise ValueError("No factorial of a negative number")
 
-    for i in range(1, n+1):
-	    result = result * i
-	
+    for i in range(1, n + 1):
+        result = result * i
+
     return result
+
 
 def nCr(n, r):
     """helper function for pascal_triangle
   Computes nCr which is useful in creating the triangle
 """
-    return int(factorial(n) / (factorial(r)* factorial(n-r))) 
-
-def pascal_triangle(n):
-  """Creates the pascal triangle with n rows
-  """
-    for i in range(j):
-        pascal_list.append((list((nCr(i, j) for j in range(0, i+1)))))
-    return pascal_list
+    return int(factorial(n) / (factorial(r) * factorial(n - r)))
